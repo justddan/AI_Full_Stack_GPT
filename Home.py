cc3117c800d8fea5c56e6d576ab03c7f2bce4278
김동규 <dgorzldd@gmail.com>
2025-03-21 15:12:06 +0900

    Streamlit은 데이터 변경이 있을 때마다 전체가 바뀐다.

--- Home.py
+++ Home.py
@@ -1,26 +1,18 @@
 import streamlit as st
-from langchain.prompts import PromptTemplate
+from datetime import datetime
 
-st.write("hello")
+today = datetime.today().strftime("%H:%M:%S")
 
-st.write([1,2,3,4])
+st.title(today)
 
-st.write({"a": 1, "b": 2})
+model = st.selectbox("Choose your model", ["gpt-4o", "gpt-3.5-turbo"])
 
-st.write(PromptTemplate)
+st.write(model)
 
-p = PromptTemplate.from_template("xxxx")
+name = st.text_input("Enter your name")
 
-st.write(p)
+st.write(name)
 
-a = [1,2,3,4,5]
+value = st.slider("temperature", min_value=0.1, max_value=1.0),
 
-d = {"a": 1, "b": 2}
-
-a
-
-d
-
-p
-
-st.selectbox("Choose your model", ["gpt-4o", "gpt-3.5-turbo"])
+st.write(value),
