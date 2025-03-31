aa8a784db2bdf479a507b051754c3e7be50264e3
김동규 <dgorzldd@gmail.com>
2025-03-31 15:54:48 +0900

    질문 프롬프트 추가

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -1,12 +1,15 @@
-import streamlit as st
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.retrievers import WikipediaRetriever
 from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.callbacks import StreamingStdOutCallbackHandler
+import streamlit as st
+from langchain.retrievers import WikipediaRetriever
+
 
 st.set_page_config(
     page_title="QuizGPT",
-    page_icon="❓"
+    page_icon="❓",
 )
 
 st.title("QuizGPT")
@@ -14,50 +17,101 @@ st.title("QuizGPT")
 llm = ChatOpenAI(
     temperature=0.1,
     model="gpt-3.5-turbo-1106",
+    streaming=True,
+    callbacks=[StreamingStdOutCallbackHandler()],
 )
 
-@st.cache_data(show_spinner="Embedding file...")
+@st.cache_data(show_spinner="Loading file...")
 def split_file(file):
     file_content = file.read()
     file_path = f"./.cache/quiz_files/{file.name}"
     with open(file_path, "wb") as f:
         f.write(file_content)
-
     splitter = CharacterTextSplitter.from_tiktoken_encoder(
         separator="\n",
         chunk_size=600,
         chunk_overlap=100,
     )
-
     loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
     return docs
 
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
 with st.sidebar:
     docs = None
-    choice = st.selectbox("Choose what you want to use.", (
-        "File", "Wikipedia Artice",
-    ),)
+    choice = st.selectbox(
+        "Choose what you want to use.",
+        (
+            "File",
+            "Wikipedia Article",
+        ),
+    )
     if choice == "File":
-        file = st.file_uploader("Upload a .docs, .txt or .pdf file", type=["docs", "txt", "pdf"])
+        file = st.file_uploader(
+            "Upload a .docx , .txt or .pdf file",
+            type=["pdf", "txt", "docx"],
+        )
         if file:
             docs = split_file(file)
     else:
         topic = st.text_input("Search Wikipedia...")
         if topic:
-            retriver = WikipediaRetriever(top_k_results=5)
+            retriever = WikipediaRetriever(top_k_results=5)
             with st.status("Searching Wikipedia..."):
-                docs = retriver.get_relevant_documents(topic)
+                docs = retriever.get_relevant_documents(topic)
+
 
 if not docs:
     st.markdown(
         """
-            Welcome to QuizGPT.
-
-            I will make a quiz from Wikipedia articles or files you upload to test your knowledge and help you study.
-
-            Get started by uploading a file or searching on Wikipedia in the sidebar.
-        """
+    Welcome to QuizGPT.
+                
+    I will make a quiz from Wikipedia articles or files you upload to test your knowledge and help you study.
+                
+    Get started by uploading a file or searching on Wikipedia in the sidebar.
+    """
     )
 else:
-    st.write(docs)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """
+    You are a helpful assistant that is role playing as a teacher.
+         
+    Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
+    
+    Each question should have 4 answers, three of them must be incorrect and one should be correct.
+         
+    Use (o) to signal the correct answer.
+         
+    Question examples:
+         
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+         
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+         
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+         
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+         
+    Your turn!
+         
+    Context: {context}
+""",
+            )
+        ]
+    )
+
+    chain = {"context": format_docs} | prompt | llm
+
+    start = st.button("Generate Quiz")
+
+    if start:
+        chain.invoke(docs)
