910c7df964c9ef0cbfb33791939ead4907fe3a5c
김동규 <dgorzldd@gmail.com>
2025-03-31 14:56:00 +0900

    WikipediaRetriever

---
+++ pages/03_QuizGPT.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.retrievers import WikipediaRetriever
+
+st.set_page_config(
+    page_title="QuizGPT",
+    page_icon="❓"
+)
+
+st.title("QuizGPT")
+
+@st.cache_data(show_spinner="Embedding file...")
+def split_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/quiz_files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
+
+with st.sidebar:
+    choice = st.selectbox("Choose what you want to use.", (
+        "File", "Wikipedia Artice",
+    ),)
+    if choice == "File":
+        file = st.file_uploader("Upload a .docs, .txt or .pdf file", type=["docs", "txt", "pdf"])
+        if file:
+            docs = split_file(file)
+    else:
+        topic = st.text_input("Search Wikipedia...")
+        if topic:
+            retriver = WikipediaRetriever(top_k_results=5)
+            with st.status("Searching Wikipedia..."):
+                docs = retriver.get_relevant_documents(topic)
